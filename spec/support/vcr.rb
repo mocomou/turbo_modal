6ea8a58e142ba544bf0f2028d6de3820e8dc14b4
tsuda <tsuda@daas.co.jp>
2023-03-09 20:14:17 +0900

    first commit

---
+++ spec/support/vcr.rb
@@ -0,0 +1,10 @@
+# require 'vcr'
+#
+# VCR.configure do |c|
+#   c.cassette_library_dir = 'spec/cassettes'
+#   c.hook_into :webmock
+#   c.default_cassette_options = {
+#     re_record_interval: 3600 # キャッシュ期間を１時間にする
+#   }
+#   c.configure_rspec_metadata!
+# end
---
+++ spec/support/webmock.rb
@@ -0,0 +1,5 @@
+require 'webmock/rspec'
+
+# Capybaraがローカルホストの通信を行うので、当該通信を許可する
+WebMock.disable_net_connect!(allow_localhost: true,
+                             allow: 'chromedriver.storage.googleapis.com')
